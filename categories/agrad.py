Fixed major aouts datasource bug + gradient outs reversal bug

categories/agrad.py
@@ -22,16 +22,10 @@ class agradExperimentTracker(categoryExperimentTracker) :
     def track(self) -> torch.Tensor :
         
         grads_this_epoch = self.xpi.anet_model.activation_grads 
-        
-        for layer_index, grad in enumerate( grads_this_epoch ) :
-            if grad.numel() == 0 :
-                raise ValueError(f"Layer {layer_index} of activation grad data not initialised")
-        
-        # It's backpropagation so we will always get them back-to-front
-        grads_this_epoch.reverse()
-        
-        # Marginalise over batch size, we're not interested in tracking this; at most per neuron
-        mean_grads_this_epoch = [grad.nanmean(dim = 0).detach().cpu() for grad in grads_this_epoch]
+  
+        # Marginalise over batch size, we're not interested in tracking this; at most per neuron.
+        # Must reverse because backpropagation goes back to front, or else we'd get the wrong order for layers
+        mean_grads_this_epoch = [grad.nanmean(dim = 0).detach().cpu() for grad in reversed(grads_this_epoch.values())]
         
         padded_grads = pad_torch_stack(mean_grads_this_epoch, pad_with = torch.nan)
         

categories/aouts.py
@@ -21,14 +21,10 @@ class aoutsExperimentTracker(categoryExperimentTracker) :
     
     def track(self) -> torch.Tensor :
         
-        outs_this_epoch = self.xpi.anet_model.activation_grads 
-        
-        for layer_index, out in enumerate( outs_this_epoch ) :
-            if out.numel() == 0 :
-                raise ValueError(f"Layer {layer_index} of activation output data not initialised")
+        outs_this_epoch = self.xpi.anet_model.activation_outs
         
         # Marginalise over batch size, we're not interested in tracking this; at most per neuron
-        mean_outs_this_epoch = [out.nanmean(dim = 0).detach().cpu() for out in outs_this_epoch]
+        mean_outs_this_epoch = [out.nanmean(dim = 0).detach().cpu() for out in outs_this_epoch.values()]
         padded_outs = pad_torch_stack(mean_outs_this_epoch, pad_with = torch.nan)
         
         # List dimension is over layers, so need dim = 0

networks.py
@@ -22,14 +22,19 @@ class ActivationNetwork(ABC, nn.Module) :
         return self._structure
     
     def clear_activation_data(self) -> None :
-        self.activation_grads : list[torch.Tensor] = [torch.Tensor([]) for _ in range(self.length)]
-        self.activation_outs : list[torch.Tensor] = [torch.Tensor([]) for _ in range(self.length)]
+        self.activation_grads : dict[int, torch.Tensor] = {}
+        self.activation_outs : dict[int, torch.Tensor] = {}
     
     def create_activation_hook(self, layer_index : int = 0) -> Callable :
         
         def layer_activation_hook(module, inp, out) -> None :
             if self.training :
-                self.activation_outs[layer_index] = out[0].detach().cpu()      
+                
+                if isinstance(out, tuple) :
+                    result = out[0].detach().cpu()   
+                else : 
+                    result =  out.detach().cpu()     
+                self.activation_outs[layer_index] = result     
         
         return layer_activation_hook
     
@@ -38,7 +43,11 @@ class ActivationNetwork(ABC, nn.Module) :
         def layer_activation_grad_hook(module, grad_in, grad_out) -> None :
             # Denied from using type hints due to unreasonable type structure - torch hooks really are a mess
             if self.training :
-                self.activation_grads[layer_index] = grad_out[0].detach().cpu()
+                
+                if isinstance(grad_out, tuple) :
+                    self.activation_grads[layer_index] = grad_out[0].detach().cpu()
+                else :
+                    self.activation_grads[layer_index] = grad_out.detach().cpu()
                 
         return layer_activation_grad_hook
 